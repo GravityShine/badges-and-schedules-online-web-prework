07d5dca9975e603ad990c7821532d3007c3c101f
Samuel Kovacs <samuelbkovacs@gmail.com>
2019-03-25 18:13:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,13 +12,13 @@ end
 
 
 def assign_rooms(array1)
-	array1.each_with_index.map do |name, index| 
+	array1.each_with_index.map do |name, index|
 		"Hello #{name}! You'll be assigned to room #{index + 1}!"
 	end
 end
 
 def printer(guests)
-  
+
   batch_badge_creator(guests).each do |badge|
     puts badge
   end
